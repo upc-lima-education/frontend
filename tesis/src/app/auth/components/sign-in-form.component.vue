a139d2711be923b642f46a8b990a3ab1a6b88ce4
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2026-01-08 17:14:08 -0500

    style: improved base css

--- tesis/src/app/auth/components/sign-in-form.component.vue
+++ tesis/src/app/auth/components/sign-in-form.component.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
+import { ref } from 'vue';
 
-    const username = ref("");
-    const password = ref("");
+const username = ref("");
+const password = ref("");
 
-    function OnSignIn(){
-        //Logic for sign-in
-    }
+function OnSignIn() {
+    //Logic for sign-in
+}
 
 </script>
 
 <template>
-    <div class="form-container">
-        <form class="sign-in-form">
+    <div class="sign-in-form-container">
+        <h1>Logo (Temporary)</h1>
+        <form class="form-container" @submit.prevent="OnSignIn">
             <input type="email" placeholder="Email" v-model="username" />
             <input type="password" placeholder="Password" v-model="password" />
             <button type="submit">Sign In</button>
@@ -21,5 +22,26 @@
 </template>
 
 <style scoped>
+.sign-in-form-container {
+    width: 100%;
+    height: 100%;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
+    gap: 2rem;
+}
+
+.form-container {
+    width: 80%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-self: center;
+    background-color: rgb(183, 187, 255);
+    /* test */
+}
 </style>
