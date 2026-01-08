6128be3b37d2fe2c3610817d43b4ff4868846a18
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2026-01-08 15:42:06 -0500

    style: changed the css default variables

---
+++ tesis/src/app/auth/components/auth-image.component.vue
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<template>
+    <div class="auth-image">
+        <!-- temp
+        <img src="@/assets/auth-image.png" alt="Authentication Image" />
+        -->
+    </div>
+</template>
+
+<style scoped>
+.auth-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background-color: var(--main-color); /* Temporary background color */
+}
+</style>
---
+++ tesis/src/app/auth/components/sign-in-form.component.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<template>
+    <div class="form-container">
+        <form class="sign-in-form">
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+        </form>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
