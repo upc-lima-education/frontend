c1520b9855359bfdc4d69c3cfde623b3cfb051be
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2026-01-08 21:17:28 -0500

    feat: added basic not found page

---
+++ tesis/src/app/public/not_found/pages/not-found.page.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="not-found-page">
+        <!--TBD: Improve page-->
+        <h1>404 - Page Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+    </div>
+</template>
+
+<style scoped>
+.not-found-page {
+    text-align: center;
+    padding: 2rem;
+}
+</style>
--- tesis/src/router/index.ts
+++ tesis/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignInPage from '@/app/auth/pages/sign-in.page.vue';
 import SignUpPage from '@/app/auth/pages/sign-up.page.vue';
+import NotFoundPage from '@/app/public/not_found/pages/not-found.page.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,7 @@ const router = createRouter({
     { path: '/', redirect: '/sign-in' },
     { path: '/sign-up', name: 'sign-up', component: SignUpPage },
     { path: '/sign-in', name: 'sign-in', component: SignInPage },
-    //{ path: '/about', name: 'about', component: () => import('../views/AboutView.vue'), },
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundPage }, //If no route is matched
   ],
 })
 
