f669a48c6581f24e8ca9937c20343cc29aaf111e
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2026-03-26 11:49:47 -0500

    chore: removed unused component

--- tesis/src/App.vue
+++ tesis/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterView, useRouter } from 'vue-router'
+import { RouterView } from 'vue-router'
 import SidebarComponent from './app/shared/components/sidebar.component.vue';
 import { ROUTE_CONSTANTS } from './app/shared/router/route-constants';
 const viewsWithoutSidebar = [
