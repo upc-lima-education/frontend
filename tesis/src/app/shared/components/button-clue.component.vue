fcaef7cea60046c2bcffa1632bef1681e20d5f38
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2026-04-07 22:13:42 -0500

    feat: added button clue to improve readability of the page

---
+++ tesis/src/app/shared/components/button-clue.component.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import TooltipComponent from './tooltip.component.vue';
+defineProps({
+    text: { type: String },
+    position: { type: String, default: 'top' }
+});
+</script>
+
+<template>
+    <TooltipComponent :text="text" :position="position">
+        <button class="btn-rounded"> ? </button>
+    </TooltipComponent>
+</template>
+
+<style scoped>
+.btn-rounded {
+    width: 25px;
+    height: 25px;
+    padding: 0;
+    border-radius: 50%;
+}
+</style>
